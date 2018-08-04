ea51a6c93260fddd5755149555d38ddeb8f1cf35
Asdrubal Hernandez <maxilive4@gmail.com>
2018-08-03 20:19:16 -0500

    Organizando estructura de los Layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,65 +21,8 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar color-nav navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <i class="fas fa-user-ninja"></i>  {{ config('app.name', 'Laravel') }} 
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+        @include('layouts.parts.nav')
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}"><i class="fas fa-user-astronaut"></i> {{ __('Login') }} </a> 
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}"><i class="fas fa-clipboard-list"></i> {{ __('Register') }} </a>
-                            </li>
-                        @else
-                            
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('tags.index') }}"><i class="fas fa-tags"></i> Etiquetas </a> 
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('categories.index') }}"><i class="far fa-bookmark"></i> Categorías </a> 
-                            </li>
-                            <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('posts.index') }}"><i class="fas fa-clipboard"></i> Posts </a> 
-                                </li>
-
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                                      {{ __('Logout') }} <i class="fas fa-door-closed"></i>
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
         @if(session('info'))
             <div class="container" style="margin-top:10px;">
                 <div class="row">
@@ -114,5 +57,7 @@
     
     <script src="{{ asset('js/app.js') }}"></script>
     @yield('scripts')
+
+    @include('layouts.parts.footer')
 </body>
 </html>
